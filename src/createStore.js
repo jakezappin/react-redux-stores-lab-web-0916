219f5aaaff39dfa52f0a6ceb155ba40fc7aa4952
jakezappin <jake.zappin@gmail.com>
2016-11-21 15:21:31 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,3 +1,20 @@
 export default function createStore(reducer){
-// your code here!
+  let state = [];
+  let listeners = [];
+
+  const getState = () => {
+    return state;
+  }
+
+  const subscribe = (fnc) => {
+      listeners.push(fnc)
+  }
+
+  const dispatch = (action) => {
+      state = reducer(state, action)
+      listeners.forEach(listener => listener())
+  }
+
+
+  return {getState: getState, subscribe: subscribe, dispatch: dispatch}
 }
